51ff96624546a87970b5fbdadc2d126c75a8edfd
Bohdan_Zaharkiv <bohdandarkzolochiv@gmail.com>
2016-10-11 18:44:56 +0300

    Persistance configurations changed

--- mentorship2-persistance/src/main/java/com/epam/mentorship/dao/jpa/MentorJpaDao.java
+++ mentorship2-persistance/src/main/java/com/epam/mentorship/dao/jpa/MentorJpaDao.java
@@ -1,12 +1,10 @@
 package com.epam.mentorship.dao.jpa;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.persistence.Query;
 
 import com.epam.mentorship.dao.MentorDao;
-import com.epam.mentorship.model.Mentee;
 import com.epam.mentorship.model.Mentor;
 
 public class MentorJpaDao extends GenericJpaDao<Mentor, Long> implements MentorDao {
@@ -21,18 +19,9 @@ public class MentorJpaDao extends GenericJpaDao<Mentor, Long> implements MentorD
 	public List<Mentor> getMentorsByMenteesAmount(int menteesAmount, boolean onlyActive) {
 		Query query = getEntityManager()
 				.createQuery("Select m from " + getPersistanceClass().getSimpleName()
-						+ " m where size(m.mentees) > :menteesAmount", Mentor.class)
-				.setParameter("menteesAmount", menteesAmount);
-		List<Mentor> mentors = query.getResultList();
-		return mentors.stream().filter(m -> {
-			for (Mentee mentee : m.getMentees()) {
-				if (Boolean.compare(onlyActive, mentee.isActive()) != 0) {
-					return false;
-				}
-			}
-
-			return true;
-		}).collect(Collectors.toList());
+						+ " m where size(m.mentees) > :menteesAmount and m.mentees.active = :onlyActive", Mentor.class)
+				.setParameter("menteesAmount", menteesAmount).setParameter("onlyActive", onlyActive);
+		return query.getResultList();
 	}
 
 }
